31c7531a16031e49a7d9bd5c75ee41cb3b99d3e7
Sergey_Oleynik <tiktaalik@yandex.ru>
2025-08-04 20:07:55 +0300

    04082025--1

--- delta-viewer-solo/src/App.tsx
+++ delta-viewer-solo/src/App.tsx
@@ -1,53 +1,67 @@
-
-import { Viewer, XKTLoaderPlugin, BCFViewpointsPlugin } from "@xeokit/xeokit-sdk"
-import './App.css'
-import { useEffect, useRef } from 'react'
-import useViewStore from "./view-store"
-
-
+import {
+  Viewer,
+  XKTLoaderPlugin,
+  BCFViewpointsPlugin,
+} from "@xeokit/xeokit-sdk";
+import "./App.css";
+import { useEffect, useRef } from "react";
+import useViewStore from "./view-store";
+interface View {
+   look: [number, number, number],
+    eye: [number, number, number],
+    up: [number, number, number]
+}
 function App() {
-  const theElement = useRef(null)
-let viewer: any = undefined
-  const { view, setView } = useViewStore()
-  let bcfViewpoints: any = undefined
-  const getView = function () {
-    if (bcfViewpoints) {
-     /*  console.log(bcfViewpoints.getViewpoint({ snapshot: false })) */
-     
-      setView(bcfViewpoints.getViewpoint({ snapshot: false }))
-      console.log(view)
-    }
+  const theElement = useRef(null);
+  let viewer: any = undefined;
+  const { view, setView } = useViewStore();
+  
+function fixView(view: View) {
+   setView(view);
   }
-   
 
   useEffect(() => {
-    
     viewer = new Viewer({
-      canvasElement: theElement.current
-    })
-
-    const xktLoaderPlugin = new XKTLoaderPlugin(viewer)
-    bcfViewpoints = new BCFViewpointsPlugin(viewer)
-
+      canvasElement: theElement.current,
+    });
+    const xktLoaderPlugin = new XKTLoaderPlugin(viewer);
+    
     const theScene = xktLoaderPlugin.load({
       id: "the-id",
       src: "geometry.xkt",
-      edges: true
-    })
+      edges: true,
+    });
     theScene.on("loaded", () => {
-      console.log(theElement.current)
-    })
-  })
-  useEffect(() => { console.log('изменения!' + new Date())}, [viewer])
+      console.log(viewer.camera);
+    });
+  });
+  useEffect(
+    () => {
+      fixView({ look: viewer.camera.look,
+     eye: viewer.camera.eye,
+     up: viewer.camera.up,});
+    }
+  );
   return (
     <>
-      <button onClick={getView} className="absolute top-0 left-1/2 z-20">ВИД!</button>
-      <canvas id="the-canvas" className="absolute h-screen w-screen z-10" ref={theElement} ></canvas>
-      <div className="absolute top-0 right-0 z-20 h-[300px] w-[300px] bg-red-100">
-    
-      </div>
+      <button
+        onClick={() => {
+          console.log(viewer.camera.eye),
+            console.log(viewer.camera.look),
+            console.log(viewer.camera.up);
+        }}
+        className="absolute top-0 left-1/2 z-20"
+      >
+        ВИД!
+      </button>
+      <canvas
+        id="the-canvas"
+        className="absolute h-screen w-screen z-10"
+        ref={theElement}
+      ></canvas>
+      <div className="absolute top-0 right-0 z-20 h-[300px] w-[300px] bg-red-100"></div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- delta-viewer-solo/src/view-store.ts
+++ delta-viewer-solo/src/view-store.ts
@@ -1,6 +1,25 @@
 import { create } from 'zustand'
- const useViewStore = create((set)=> ({
-    view: undefined,
-    setView: ((newView: any)=> set({view: newView}))
-  }) )
+
+interface View {
+   look: [number, number, number],
+    eye: [number, number, number],
+    up: [number, number, number]
+}
+
+interface ViewerState {
+  view: {
+    look: [number, number, number],
+    eye: [number, number, number],
+    up: [number, number, number]
+  }
+  setView: (newView: View) => void
+}
+const useViewStore = create<ViewerState>((set)=> ({
+    view: {
+      look: [0, 0, 0],
+      eye: [0, 0, 0],
+      up: [0, 0, 0]
+    },
+    setView: ((newView: View)=> set({view: newView}))
+  })  )
   export default useViewStore
