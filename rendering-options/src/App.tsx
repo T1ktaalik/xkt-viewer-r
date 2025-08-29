fc0dad2a725d96f45d97e8f1bb685cd2743cf287
Sergey_Oleynik <tiktaalik@yandex.ru>
2025-08-29 18:57:45 +0300

    29082025--1

--- rendering-options/src/App.tsx
+++ rendering-options/src/App.tsx
@@ -1,14 +1,41 @@
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import {
+  Viewer,
+  XKTLoaderPlugin,
+} from "@xeokit/xeokit-sdk";
 function App() {
   const [count, setCount] = useState(0)
+  const theCanvas = useRef(null)
+  const viewer: React.RefObject<any> = useRef(null)
+
+  useEffect(()=> {
+    viewer.current = new Viewer({
+      canvasElement: theCanvas.current
+    })
+    const xktLoader = new XKTLoaderPlugin(viewer.current)
+    const model = xktLoader.load({
+      id: "maz",
+      src: "/models/ar/geometry.xkt",
+      edges: false
+    })
+
+    model.on("loaded", () => {
+      console.log("model loaded")
+       viewer.current.cameraFlight.flyTo(model)
+    })
+  }, [])
+
 
   return (
     <>
-    <div className=''></div>
+    <div className="h-screen w-screen overflow-hidden">
+      <canvas className="h-full w-full" ref={theCanvas}>
+
+      </canvas>
+    </div>
     </>
   )
 }
