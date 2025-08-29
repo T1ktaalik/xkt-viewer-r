36412c10ff54acb4f71ced3c175a2edd0f0696bf
Sergey_Oleynik <tiktaalik@yandex.ru>
2025-08-29 19:15:41 +0300

    29082025--2-1

--- rendering-options/src/App.tsx
+++ rendering-options/src/App.tsx
@@ -5,6 +5,8 @@ import './App.css'
 import {
   Viewer,
   XKTLoaderPlugin,
+   AmbientLight,
+  DirLight
 } from "@xeokit/xeokit-sdk";
 function App() {
   const [count, setCount] = useState(0)
@@ -15,19 +17,52 @@ function App() {
     viewer.current = new Viewer({
       canvasElement: theCanvas.current
     })
+
+    viewer.current.scene.clearLights()
+
+    new AmbientLight(viewer.current.scene, {
+     color: [0.999, 0.999, 0.999],
+     intensity: 0.2
+});
+
+new DirLight(viewer.current.scene, {
+     id: "keyLight",
+     dir: [0.8, -0.6, -0.8],
+     color: [1.0, 0.999, 0.999],
+     intensity: 1.0,
+     space: "view"
+});
+/* 
+new DirLight(viewer.current.scene, {
+     id: "fillLight",
+     dir: [-0.8, -0.4, -0.4],
+     color: [0.3, 1.0, 0.3],
+     intensity: 1.0,
+     space: "view"
+});
+
+new DirLight(viewer.current.scene, {
+     id: "rimLight",
+     dir: [0.2, -0.8, 0.8],
+     color: [0.6, 0.6, 0.6],
+     intensity: 1.0,
+     space: "view"
+}); */
+
+
     const xktLoader = new XKTLoaderPlugin(viewer.current)
     const model = xktLoader.load({
       id: "maz",
       src: "/models/ar/geometry.xkt",
-      edges: false
+      edges: true
     })
 
     model.on("loaded", () => {
-      console.log("model loaded")
-       viewer.current.cameraFlight.flyTo(model)
+      viewer.current.cameraFlight.flyTo(model)
     })
   }, [])
-
+  
+  
 
   return (
     <>
